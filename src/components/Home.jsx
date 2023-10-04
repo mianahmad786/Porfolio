fb2ea532c8ae2995f47066e65dffa44217362d1e
Ahmad Ibrahim <ahmadibrahim8103033@gmail.com>
2023-10-04 13:32:19 +0500

    code update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
             <a
               name="resume"
               target="_blank"
-              href="https://drive.google.com/file/d/1efvDJTqPdt1FXHj4pGCPA-ulK7GXjsQs/view?usp=drivesdk"
+              href="https://drive.google.com/file/d/1f0eVe2uyUZL0pQhdhdR_ZHPtIzjE-D_S/view?usp=drivesdk"
               className="flex group justify-start items-center gap-3  w-fit py-3 cursor-pointer px-6 my-2 rounded-xl border"
             >
               My Resume{" "}
